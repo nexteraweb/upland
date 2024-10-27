3889f8b95ec9cb4928cd1778f8301010805b9e66
akash100a <arakash968786@gmail.com>
2024-10-27 23:35:41 +0600

    Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,9 @@
 // Jodi hook(Like useState, useEffect) use kora lage tahole use client taa uncomment kore dio karon default server component next js a ,,,
 export default function Home() {
   return (
-   <>
-    <h1>Hellow NextEraWeb1</h1>
-   </>
+    <>
+      <h1>Hellow NextEraWeb1</h1>
+      <p></p>
+    </>
   );
 }
