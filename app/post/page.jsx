1007adc68ba710669ad1e8a4f74c95902cf58a48
Abdul Kadir <abdulkadir112k@gmail.com>
2024-10-28 01:08:43 +0600

    post part updated

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,10 +1,16 @@
-// "use client"
+"use client"
+import Image from 'next/image'
 // Jodi hook(Like useState, useEffect) use kora lage tahole use client taa uncomment kore dio karon default server component next js a ,,,
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
     return (
-        <div>Post page</div>
+        <div>
+         
+         <Link href='#'>Ho</Link>
+         {/* <Image src={Logo} alt='Logo' width={100} height={100}/> */}
+        </div>
     )
 }
 
