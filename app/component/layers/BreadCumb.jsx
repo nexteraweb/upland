02fda27b658a41b2e7f5f21ee76e7524e64aa39b
akash100a <arakash968786@gmail.com>
2024-10-28 21:12:26 +0600

    BreadCumb Change

--- app/component/layers/BreadCumb.jsx
+++ app/component/layers/BreadCumb.jsx
@@ -7,10 +7,7 @@ const BreadCumb = ({className,three}) => {
     const pathname = usePathname()
   return (
     <>  <div className={` ${className}`}>
-            {/* <h2 className='font-bold text-[3.06rem] leading-none text-[#262626] capitalize font-dm'>{window.location.pathname.split("/")[1]}</h2>
-            <p className='mt-3 font-normal text-xs text-[#767676] font-dm capitalize'>Home <MdKeyboardArrowRight className='inline-block'/>
-            {window.location.pathname.split("/")[1]}  <span className={`${three}`}><MdKeyboardArrowRight className='inline-block'/> {window.location.pathname.split("/")[2]}</span></p> */}
-            <p>{pathname}</p>
+            <p>Home - {pathname.split("/")[1]}</p>
         </div>
     </>
   )
