3cea464cf1b0dd8695147e85167cd6d3fda34964
akash100a <arakash968786@gmail.com>
2024-10-28 21:15:24 +0600

    BreadCumb

--- app/component/layers/BreadCumb.jsx
+++ app/component/layers/BreadCumb.jsx
@@ -1,13 +1,13 @@
 'use client'
 import React from 'react'
-// import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 import { usePathname } from 'next/navigation'
 
 const BreadCumb = ({className,three}) => {
     const pathname = usePathname()
   return (
     <>  <div className={` ${className}`}>
-            <p>Home - {pathname.split("/")[1]}</p>
+            <p>Home <MdKeyboardArrowRight className='inline-block'/>{pathname.split("/")[1]}</p>
         </div>
     </>
   )
