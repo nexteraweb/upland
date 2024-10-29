fbc6bf6a0d5a4890119840aeffd77c360aaa434c
Abdul Kadir <abdulkadir112k@gmail.com>
2024-10-30 02:44:59 +0600

    navbar and breadcumb done

--- app/component/layers/BredCumb.jsx
+++ app/component/layers/BredCumb.jsx
@@ -1,19 +1,20 @@
+'use client'
 import React from 'react'
 import '../../post/post.css'
 import Link from 'next/link'
 import { codystar, roboto } from '@/app/layout'
-import Image from 'next/image'
-import navBg2 from '../../../public/navBg2.png'
+import { usePathname } from 'next/navigation'
 
 const BredCumb = () => {
+  const pathname = usePathname()
   return (
-    <div className='pt-[159px] pb-[181px]'>
-        <h2 className='font-Darker font-semibold text-6xl text-white capitalize'>{window.location.pathname.split("/") [1]}</h2>
+    <div className='relative pt-[159px] pb-[181px]'>
+        <h2 className='font-Darker font-semibold text-6xl text-white capitalize'>{pathname.split("/") [1]}</h2>
         <div className='flex gap-x-3 pt-[30px]'>
           <Link className={`${roboto.className} font-normal text-sm text-[#B3B1B7]`} href={'/'}>Home</Link>
-          <p className={`${roboto.className} font-normal text-sm text-[#B3B1B7] capitalize`}>{window.location.pathname.split("/")[1]}</p>
+          <p className={`${roboto.className} font-normal text-sm text-[#B3B1B7] capitalize`}>{pathname.split("/")[1]}</p>
         </div>
-        <h3 className={`${codystar.className} font-normal text-[120px] uppercase text-[#CCCCCC] absolute left-[71px] top-[230px]`}>{window.location.pathname.split("/")[1]}</h3>
+        <h3 className={`${codystar.className} font-normal text-[120px] uppercase text-[#CCCCCC] absolute right-full top-[230px]`}>{pathname.split("/")[1]}</h3>
     </div>
   )
 }
--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,5 +1,4 @@
 // "use client"
-import Image from 'next/image'
 // Jodi hook(Like useState, useEffect) use kora lage tahole use client taa uncomment kore dio karon default server component next js a ,,,
 import Link from 'next/link'
 import React from 'react'
@@ -8,10 +7,10 @@ import Container from '../component/layers/Container'
 
 const page = () => {
     return (
-        <div>
-         
-         <Link href='#'>Ho</Link>
-         {/* <Image src={Logo} alt='Logo' width={100} height={100}/> */}
+        <div className='navBg2'>
+         <Container>
+            <BredCumb />
+         </Container>
         </div>
     )
 }
