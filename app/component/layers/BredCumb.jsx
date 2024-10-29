5f9c05d2d72e07e44dfa761ec6ef71f00884516c
Abdul Kadir <abdulkadir112k@gmail.com>
2024-10-29 22:34:45 +0600

    updete bredCumb

--- app/component/layers/BredCumb.jsx
+++ app/component/layers/BredCumb.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BredCumb = () => {
   return (
     <div>
-        <h2 className='font-Darker font-semibold text-6xl text-white'>{window.location.pathname.split("/") [1]}</h2>
+        <h2 className='font-Darker font-semibold text-6xl text-white capitalize'>{window.location.pathname.split("/") [1]}</h2>
     </div>
   )
 }
