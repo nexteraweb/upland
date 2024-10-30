78ff5312297717ab55f42cc2752527fa9d2717e9
akash100a <arakash968786@gmail.com>
2024-10-30 23:11:30 +0600

    up

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,7 +1,7 @@
 // "use client"
 // Jodi hook(Like useState, useEffect) use kora lage tahole use client taa uncomment kore dio karon default server component next js a ,,,
 import React from 'react'
-import BredCumb from '../component/layers/BredCumb'
+import BredCumb from '@/app/component/layers/BredCumb'
 import PostContent from '../component/PostContent'
 
 const page = () => {
